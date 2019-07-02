a5a40c2adfd224a2672773ef4bbb976c0ef3c087
Jason Adams <jasonadams023@gmail.com>
2019-07-02 12:21:48 -0500

    Makes portfolio.js independent of race conditions

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,20 @@
-$('#portfolio-panel .collapse').on('show.bs.collapse', function (e) {
-    var actives = $('#portfolio-panel').find('.in, .collapsing');
-    actives.each( function (index, element) {
-        $(element).collapse('hide');
+var intervalID;
+
+function setPortfolio() {
+    var panels = $('#portfolio-panel .collapse');
+
+    if (panels.length == 0) {
+        return;
+    }
+
+    panels.on('show.bs.collapse', function (e) {
+        var actives = $('#portfolio-panel').find('.in, .collapsing');
+        actives.each( function (index, element) {
+            $(element).collapse('hide');
+        })
     })
-})
+
+    clearInterval(intervalID);
+}
+
+intervalID = setInterval(setPortfolio, 500);
