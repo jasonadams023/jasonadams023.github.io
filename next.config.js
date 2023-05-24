cfd4f9413093c05d3674c0d9f0cf41f89e0f3ca0
Jason Adams <jason@spacee.com>
2023-05-24 15:32:30 -0500

    Removes extra path configs

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = '/'
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 const nextConfig = {
   experimental: {
